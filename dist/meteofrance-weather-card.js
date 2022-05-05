895e42ee59932c50f04b710a92cacce9e7047160
vingerha <44190435+vingerha@users.noreply.github.com>
2022-05-05 11:31:46 +0200

    Update meteofrance-weather-card.js

--- dist/meteofrance-weather-card.js
+++ dist/meteofrance-weather-card.js
@@ -31,9 +31,9 @@ const DefaultSensors = [
 ];
 
 const weatherIconsNight = {
-  ...weatherIconsDay,
   clear: "night",
   sunny: "night",
+  clear-night: "night",
   partlycloudy: "cloudy-night-3",
   "windy-variant": "cloudy-night-3",
 };
@@ -475,6 +475,95 @@ class MeteofranceWeatherCard extends LitElement {
     const diff = new Date(forecast[1].datetime) - new Date(forecast[0].datetime);
     return diff > 3600000;
   }
+  
+  
+  renderHourlyForecast(forecast) {
+    if (!forecast || forecast.length === 0) {
+      return html``;
+    }
+
+    const lang = this.hass.selectedLanguage || this.hass.language;
+    const isDaily = this.isHourlyForecast(forecast);
+
+    this.numberElements++;
+    return html`
+      <ul class="flow-row forecast ${this.numberElements > 1 ? " spacer" : ""}">
+        ${forecast
+        .slice(
+          0,
+          this._config.number_of_forecasts
+            ? this._config.number_of_forecasts
+            : 5
+        )
+        .map(
+          (hourly) => this.renderDailyForecast2(hourly, lang, isHourly)
+        )}
+      </ul>`;
+  }
+
+  renderHourlyForecast2(hourly, lang, isHourly) {
+    return html`
+        <li>
+          <ul class="flow-column day">
+            <li>
+            ${isDaily
+        ? new Date(hourly.datetime).toLocaleDateString(lang, {
+          weekday: "short",
+        })
+        : new Date(hourly.datetime).toLocaleTimeString(lang, {
+          hour: "2-digit",
+          minute: "2-digit",
+        })}
+            </li>
+            <li class="icon" style="background: none, url('${this.getWeatherIcon(
+          hourly.condition.toLowerCase()
+        )}') no-repeat; background-size: contain">
+            </li>
+            <li class="highTemp">
+            ${hourly.temperature}${this.getUnit("temperature")}
+            </li>
+          ${hourly.templow !== undefined
+        ? html`
+            <li class="lowTemp">
+            ${hourly.templow}${this.getUnit("temperature")}
+            </li>
+          `
+        : ""}
+          ${!this._config.hide_precipitation &&
+        hourly.precipitation !== undefined &&
+        hourly.precipitation !== null
+        ? html`
+            <li class="precipitation">
+              ${Math.round(hourly.precipitation * 10) / 10} ${this.getUnit("precipitation")}
+            </li>
+          `
+        : ""}
+          ${hourly.wind_speed !== undefined &&
+		hourly.wind_speed !== null
+        ? html`
+            <li class="wind_speed">
+            ${Math.round(hourly.wind_speed * 3.6)} ${this.getUnit("speed")}
+            </li>
+          `
+        : ""}
+          </ul>
+        </li>`;
+  }
+
+  isHourlyForecast(forecast) {
+    const diff = new Date(forecast[1].datetime) - new Date(forecast[0].datetime);
+    return diff > 2600000;
+  }  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   getOneHourForecast(rainForecastEntity) {
     let rainForecastList = [];
