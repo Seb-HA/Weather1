4d2ce8d037418608f5dcc9d2b541cf8ff4a7db69
vingerha <44190435+vingerha@users.noreply.github.com>
2023-04-30 08:33:22 +0200

    Update meteofrance-weather-card.js

--- dist/meteofrance-weather-card.js
+++ dist/meteofrance-weather-card.js
@@ -559,6 +559,8 @@ class MeteofranceWeatherCard extends LitElement {
   }
 
   getWeatherIcon(condition, datetimehourly) {
+	var nextsetting = this.hass.states["sun.sun"].attributes.next_setting
+	var nextrising = this.hass.states["sun.sun"].attributes.next_rising 
     return `${this._config.icons
       ? this._config.icons
       : "/local/community/lovelace-meteofrance-weather-card/icons/"
